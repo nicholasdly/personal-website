7159573336b03df42dd1cbab177747d6ad89b89a
Nicholas Ly <nichdly@gmail.com>
2022-11-29 03:33:11 -0500

    Moved image assets to public directory

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,19 +2,19 @@
 export default [
     {
         title: 'Tomatera',
-        imgURL: 'src/assets/tomatera.png',
+        imgURL: 'assets/tomatera.png',
         techstack: ['React', 'CSS', 'JavaScript'],
         link: 'https://tomatera.dev/'
     },
     {
         title: 'AIDED Website',
-        imgURL: 'src/assets/aided.png',
+        imgURL: 'assets/aided.png',
         techstack: ['React', 'Tailwind', 'NEXT.js'],
         link: 'https://msuaided.com/'
     },
     {
         title: 'Personal Website',
-        imgURL: 'src/assets/missing.png',
+        imgURL: 'assets/missing.png',
         techstack: ['React', 'Tailwind', 'Vite'],
         link: 'https://nicholasly.com/'
     }
