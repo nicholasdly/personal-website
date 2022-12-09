7f6e564b077da0785fa33c1ce8c7f495e86ff19f
Nicholas Ly <nichdly@gmail.com>
2022-12-09 02:48:43 -0500

    Changed component file type

--- components/Role.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Role({ source, title }) {
-    return (
-        <div className="text-right text-[20px] mt-[20px]">
-            <p className="font-bold">{source}</p>
-            <p>{title}</p>
-        </div>
-    )
-}
