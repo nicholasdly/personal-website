113d76960fe8d1bad4dbad5695e87d97bce45aaf
Nicholas Ly <nichdly@gmail.com>
2022-12-10 13:08:16 -0500

    Super minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,10 @@ module.exports = {
             transform: "translate(0px, 0px) scale(1)"
           },
           "33%": {
-            transform: "translate(0px, -50px) scale(1.1)"
+            transform: "translate(30px, -50px) scale(1.1)"
           },
           "66%": {
-            transform: "translate(-40px, 20px) scale(0.9)"
+            transform: "translate(-20px, 20px) scale(0.9)"
           },
           "100%": {
             transform: "translate(0px, 0px) scale(1)"
